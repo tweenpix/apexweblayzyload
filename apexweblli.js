6e159c7704a5d503b4e6466027a37690600c16df
Apexweb <tbuhnukc@gmail.com>
2023-05-25 12:38:43 +0700

    first

---
+++ apexweblli.js
@@ -0,0 +1,54 @@
+/*
+apexweb.ru
+small lazy load library
+how to use:
+just set src attribute by placeholder and set data-src attribute in img tag and load library
+example: <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2NCA2NCIgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0Ij4KICA8cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNjY2NjY2MiPjwvcmVjdD4KICA8dGV4dCB4PSI1MCUiIHk9IjUwJSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSIgZm9udC1mYW1pbHk9Im1vbm9zcGFjZSIgZm9udC1zaXplPSIxMHB4IiBmaWxsPSIjMzMzMzMzIj5hcGV4d2ViLnJ1PC90ZXh0PiAgIAo8L3N2Zz4=" data-src="./image.png" alt="">
+
+*/
+
+
+// Функция для проверки, находится ли элемент в области просмотра
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Функция для загрузки изображения
+function loadImage(element) {
+    const src = element.getAttribute('data-src');
+    if (src) {
+        element.setAttribute('src', src);
+        element.removeAttribute('data-src');
+    }
+}
+
+// Функция для обработки события прокрутки страницы
+function handleScroll() {
+    const lazyImages = document.querySelectorAll('img[data-src]');
+
+    lazyImages.forEach((lazyImage) => {
+        if (isInViewport(lazyImage)) {
+            loadImage(lazyImage);
+        }
+    });
+}
+
+// Загрузить изображения, находящиеся в области просмотра сразу
+document.addEventListener('DOMContentLoaded', () => {
+    const initialLoadImages = document.querySelectorAll('img[data-src]');
+
+    initialLoadImages.forEach((image) => {
+        if (isInViewport(image)) {
+            loadImage(image);
+        }
+    });
+});
+
+// Обработать событие прокрутки страницы
+window.addEventListener('scroll', handleScroll);
